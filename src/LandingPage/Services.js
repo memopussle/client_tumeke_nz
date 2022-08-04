21d11f204a971153dcb44b0d562c4b7c9025a78a
thugiang <xanhtham.cuc@gmail.com>
2022-08-04 13:27:31 +1200

    fetching data and display each booking in landing page

--- src/LandingPage/Services.js
+++ src/LandingPage/Services.js
@@ -1,6 +1,6 @@
 import React from "react";
 import useStyles from "./styles.js";
-import { Typography } from "@material-ui/core";
+import { Typography, Card, CardMedia, CardContent, Grid } from "@material-ui/core";
 import StarIcon from "@material-ui/icons/Star";
 import StarBorder from "@material-ui/icons/StarBorder";
 
@@ -32,25 +32,40 @@ const Services = ({ booking }) => {
   return (
     <div>
       <Typography variant="h4">Our Tours</Typography>
-      {newBooking.map((service) => (
-        <div key={service?._id}>
-          <Typography variant="h6">{service.title}</Typography>
-          <Typography variant="body2">
-            Price from ${service.price} per {service.per}
-          </Typography>
-          <Typography variant="body2">Date: {service.date}</Typography>
-          <img src={service.img} alt="service" className={classes.serviceImg} />
-          <div>
-            <StarIcon style={{ color: "#f7981d", fontSize: "1.2rem" }} />
-            <StarIcon style={{ color: "#f7981d", fontSize: "1.2rem" }} />
-            <StarIcon style={{ color: "#f7981d", fontSize: "1.2rem" }} />
-            <StarIcon style={{ color: "#f7981d", fontSize: "1.2rem" }} />
-            <StarBorder style={{ color: "#f7981d", fontSize: "1.2rem" }} />
-            4.8
-          </div>
-          <Typography variant="body1">{service.description}</Typography>
-        </div>
-      ))}
+      <Grid container spacing={2}>
+        {newBooking.map((service) => (
+          <Grid key={service?._id} item xs={12} md={6} lg={3}>
+            <Card>
+              <CardMedia
+                src={service.img}
+                alt="service"
+                className={classes.serviceImg}
+                height="500"
+                component="img"
+              />
+              <CardContent>
+                <Typography variant="h6">{service.title}</Typography>
+                <Typography variant="body2">
+                  Price from ${service.price} per {service.per}
+                </Typography>
+                <Typography variant="body2">Date: {service.date}</Typography>
+
+                <div>
+                  <StarIcon style={{ color: "#f7981d", fontSize: "1.2rem" }} />
+                  <StarIcon style={{ color: "#f7981d", fontSize: "1.2rem" }} />
+                  <StarIcon style={{ color: "#f7981d", fontSize: "1.2rem" }} />
+                  <StarIcon style={{ color: "#f7981d", fontSize: "1.2rem" }} />
+                  <StarBorder
+                    style={{ color: "#f7981d", fontSize: "1.2rem" }}
+                  />
+                  4.8
+                </div>
+                <Typography variant="body1">{service.description}</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
     </div>
   );
 };
